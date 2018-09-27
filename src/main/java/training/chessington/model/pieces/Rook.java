20b9694108d8265e24c84b9ddecd94515ae32555
GAJEWSKI Filip <fgajewski@europe.corp.altran.com>
2018-09-27 15:54:18 +0100

    Rook Works and fully implemented, Pawn doesnt work fully

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -16,96 +16,145 @@ public class Rook extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
     	ArrayList<Move> allowed_moves = new ArrayList<Move>();
-    	allowed_moves.addAll(LeftMoves(from, board));
-    	allowed_moves.addAll(RightMoves(from, board));
-    	allowed_moves.addAll(UpMoves(from, board));
-    	allowed_moves.addAll(DownMoves(from, board));
+//    	allowed_moves.addAll(LeftMoves(from, board));
+//    	allowed_moves.addAll(RightMoves(from, board));
+//    	allowed_moves.addAll(UpMoves(from, board));
+//    	allowed_moves.addAll(DownMoves(from, board));
+    	allowed_moves.addAll(RookMoves(from, board, "left"));
+    	allowed_moves.addAll(RookMoves(from, board, "right"));
+    	allowed_moves.addAll(RookMoves(from, board, "up"));
+    	allowed_moves.addAll(RookMoves(from, board, "down"));
         return allowed_moves;
     }
 
-    private List<Move> LeftMoves (Coordinates from, Board board) {
-    	ArrayList<Move> LeftMoves = new ArrayList<Move>();
+//    private List<Move> LeftMoves (Coordinates from, Board board) {
+//    	ArrayList<Move> LeftMoves = new ArrayList<Move>();
+//    	Coordinates to;
+//    	PlayerColour enemy = Enemy(colour);
+//    	
+//    	for(int i = 1; i<=from.getCol() ; i++) {
+//    		to = new Coordinates(from.getRow(), from.getCol()-i);
+//    		
+//    		if(board.get(to)==null) {
+//    			LeftMoves.add(new Move(from, to));
+//    		}
+//    		else if(board.get(to).getColour()==enemy) {
+//    			LeftMoves.add(new Move(from, to));
+//    		}
+//    		else if(board.get(to).getColour()==colour) {
+//    		}
+//    	}
+//    	return LeftMoves;
+//    }
+//       
+//    private List<Move> RightMoves (Coordinates from, Board board) {
+//    	ArrayList<Move> RightMoves = new ArrayList<Move>();
+//    	Coordinates to;
+//    	PlayerColour enemy = Enemy(colour);
+//    	
+//    	for(int i = 1; i<=(7-from.getCol()) ; i++) {
+//    		to = new Coordinates(from.getRow(), from.getCol()+i);
+//    		
+//    		if(board.get(to)==null) {
+//    			RightMoves.add(new Move(from, to));
+//    		}
+//    		else if(board.get(to).getColour()==enemy) {
+//    			RightMoves.add(new Move(from, to));
+//    		}
+//    		else if(board.get(to).getColour()==colour) {
+//    		}
+//    	}
+//    	return RightMoves;
+//
+//    }    
+//    
+//    private List<Move> UpMoves (Coordinates from, Board board) {
+//    	ArrayList<Move> UpMoves = new ArrayList<Move>();
+//    	Coordinates to;
+//    	PlayerColour enemy = Enemy(colour);
+//    	
+//    	for(int i = 1; i<=(7-from.getRow()) ; i++) {
+//    		to = new Coordinates(from.getRow()+i, from.getCol());
+//    		
+//    		if(board.get(to)==null) {
+//    			UpMoves.add(new Move(from, to));
+//    		}
+//    		else if(board.get(to).getColour()==enemy) {
+//    			UpMoves.add(new Move(from, to));
+//    		}
+//    		else if(board.get(to).getColour()==colour) {
+//    		}
+//    	}
+//    	return UpMoves;
+//
+//    }  
+//    
+//    private List<Move> DownMoves (Coordinates from, Board board) {
+//    	ArrayList<Move> DownMoves = new ArrayList<Move>();
+//    	Coordinates to;
+//    	PlayerColour enemy = Enemy(colour);
+//    	
+//    	for(int i = 1; i<=from.getRow() ; i++) {
+//    		to = new Coordinates(from.getRow()-i, from.getCol());
+//    		
+//    		if(board.get(to)==null) {
+//    			DownMoves.add(new Move(from, to));
+//    		}
+//    		else if(board.get(to).getColour()==enemy) {
+//    			DownMoves.add(new Move(from, to));
+//    		}
+//    		else if(board.get(to).getColour()==colour) {
+//    		}
+//    	}
+//    	return DownMoves;
+//
+//    }
+
+    private List<Move> RookMoves (Coordinates from, Board board, String direction) {
+    	ArrayList<Move> RookMoves = new ArrayList<Move>();
     	Coordinates to;
     	PlayerColour enemy = Enemy(colour);
+    	int limit=0;
+    	int rowDir=0;
+    	int colDir=0;
     	
-    	for(int i = 1; i<=from.getCol() ; i++) {
-    		to = new Coordinates(from.getRow(), from.getCol()-i);
+    	if(direction.equals("left")) {
+    		limit = from.getCol();
+    		rowDir = 0;
+    		colDir = 1;
+    	}
+    	else if(direction.equals("right")) {
+    		limit = 7-from.getCol();
+    		rowDir = 0;
+    		colDir = -1;
+    	}
+    	else if(direction.equals("up")) {
+    		limit = 7-from.getRow();
+    		rowDir = -1;
+    		colDir = 0;
+    	}
+    	else if(direction.equals("down")) {
+    		limit = from.getRow();
+    		rowDir = 1;
+    		colDir = 0;
+    	}
+    	
+    	for(int i = 1; i<=limit ; i++) {
+    		to = new Coordinates(from.getRow()-(i*rowDir), from.getCol()-(i*colDir));
     		
     		if(board.get(to)==null) {
-    			LeftMoves.add(new Move(from, to));
+    			RookMoves.add(new Move(from, to));
     		}
     		else if(board.get(to).getColour()==enemy) {
-    			LeftMoves.add(new Move(from, to));
+    			RookMoves.add(new Move(from, to));
+    			break;
     		}
     		else if(board.get(to).getColour()==colour) {
+    			break;
     		}
     	}
-    	return LeftMoves;
+    	return RookMoves;
     }
-       
-    private List<Move> RightMoves (Coordinates from, Board board) {
-    	ArrayList<Move> RightMoves = new ArrayList<Move>();
-    	Coordinates to;
-    	PlayerColour enemy = Enemy(colour);
-    	
-    	for(int i = 1; i<=(7-from.getCol()) ; i++) {
-    		to = new Coordinates(from.getRow(), from.getCol()+i);
-    		
-    		if(board.get(to)==null) {
-    			RightMoves.add(new Move(from, to));
-    		}
-    		else if(board.get(to).getColour()==enemy) {
-    			RightMoves.add(new Move(from, to));
-    		}
-    		else if(board.get(to).getColour()==colour) {
-    		}
-    	}
-    	return RightMoves;
-
-    }    
-    
-    private List<Move> UpMoves (Coordinates from, Board board) {
-    	ArrayList<Move> UpMoves = new ArrayList<Move>();
-    	Coordinates to;
-    	PlayerColour enemy = Enemy(colour);
-    	
-    	for(int i = 1; i<=(7-from.getCol()) ; i++) {
-    		to = new Coordinates(from.getRow()+i, from.getCol());
-    		
-    		if(board.get(to)==null) {
-    			UpMoves.add(new Move(from, to));
-    		}
-    		else if(board.get(to).getColour()==enemy) {
-    			UpMoves.add(new Move(from, to));
-    		}
-    		else if(board.get(to).getColour()==colour) {
-    		}
-    	}
-    	return UpMoves;
-
-    }  
-    
-    private List<Move> DownMoves (Coordinates from, Board board) {
-    	ArrayList<Move> DownMoves = new ArrayList<Move>();
-    	Coordinates to;
-    	PlayerColour enemy = Enemy(colour);
-    	
-    	for(int i = 1; i<=from.getCol() ; i++) {
-    		to = new Coordinates(from.getRow()-i, from.getCol());
-    		
-    		if(board.get(to)==null) {
-    			DownMoves.add(new Move(from, to));
-    		}
-    		else if(board.get(to).getColour()==enemy) {
-    			DownMoves.add(new Move(from, to));
-    		}
-    		else if(board.get(to).getColour()==colour) {
-    		}
-    	}
-    	return DownMoves;
-
-    }
-    
     
     private PlayerColour Enemy(PlayerColour Piece) {
     	PlayerColour enemy;
